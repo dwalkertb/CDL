71735029d75f4777f873c2d52bdc16d8502063a9
Pengcheng Li <dwalkertb@gmail.com>
2017-05-23 16:30:13 +0800

    1. 添加readme 2. 表格操作链接标题摘要

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-CNLAB-协同推荐专家系统
+###CNLAB-协同推荐专家系统
+数据处理过程依据论文：**Collaborative Topic Modeling for Recommending Scientific Articles**
+
+_论文中相关描述：
+For each article, we concatenate its title and abstract. We remove stop words and
+use tf-idf to choose the top 8000 distinct words as the vocabulary 。_
+
+本实验处理流程：
+1. 给定的文件每行对应一个项目名字、专家、摘要。将项目名字和摘要链接，得到的文本用pynlpir进行中文分词，
+并且去掉停用词，用tf-idf算法，选择一定数量的词，构成最后词袋。
--- test/testxlrd.py
+++ test/testxlrd.py
@@ -8,22 +8,30 @@
 
 import xlrd
 import codecs
+import pynlpir
 
 item = []
-data = xlrd.open_workbook('C:\\Users\\TBin\\Desktop\\dataset.xlsx')
-itemname = codecs.open('C:\\Users\\TBin\\Desktop\\itemname.txt', 'a+', "utf-8")
+data = xlrd.open_workbook('../res/dataset.xlsx')
+#itemname = codecs.open('C:\\Users\\TBin\\Desktop\\itemname.txt', 'a+', "utf-8")
 table = data.sheets()[0]
 nrows = table.nrows
-for i in range(nrows):
+for i in range(5):
     if i == 0:
         continue
-    item.append(table.row_values(i)[2:3])
+    item.append(table.row_values(i)[2]+table.row_values(i)[4])
+print(item)
+print('**********************************')
 
-print(type(item))
-print(type(table.row_values(i)[2:3]))
-'''
-for n in item:
-    itemname.write(n+'\n')
-itemname.close()
+singletextResult = []
+s = ""
+s1 = "".join(item).strip().encode('utf-8')
 
-'''
+print(s1)
+print('**********************************')
+
+for item in pynlpir.segment(s1):
+    singletextResult.append(item)
+print(singletextResult)
+
+
+
