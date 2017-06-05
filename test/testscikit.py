3fa2ad5bffdfa08bc4a59487d3c72246c2346185
Pengcheng Li <dwalkertb@gmail.com>
2017-06-05 17:58:29 +0800

    修改了挺支持停止词 完成了数据清洗处理

--- test/testscikit.py
+++ test/testscikit.py
@@ -7,10 +7,6 @@
 ###########################
 '''
 
-#import jieba
-#import jieba.posseg as pseg
-#import os
-#import sys
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -22,11 +18,18 @@ if __name__ == "__main__":
               "我 爱 北京 天安门"]  # 第四类文本的切词结果
     vectorizer = CountVectorizer()  # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
     transformer = TfidfTransformer()  #  该类会统计每个词语的tf-idf权值
-    tfidf = transformer.fit_transform(
-        vectorizer.fit_transform(corpus))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
+    v = vectorizer.fit_transform(corpus)
+
+    tfidf = transformer.fit_transform(v)  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
+
     word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语
     weight = tfidf.toarray()  # 将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重
     for i in range(len(weight)):  # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
         print(u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
-    for j in range(len(word)):
-        print(word[j], weight[i][j])
+        for j in range(len(word)):
+            print(word[j], weight[i][j])
+
+    print(type(v))
+    print('*************************')
+    print(type(tfidf))
+
